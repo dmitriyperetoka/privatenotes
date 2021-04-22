72e784725ccdd4c60254c68c29a3f2df6ab71aa1
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-04-23 00:25:31 +0300

    Move note deletion procedure to server side

--- privatenotes/notes/views.py
+++ privatenotes/notes/views.py
@@ -1,3 +1,5 @@
+import requests
+from django.shortcuts import reverse
 from django.views.generic import FormView, DetailView
 
 from .forms import NoteForm
@@ -11,3 +13,11 @@ class NoteCreateView(FormView):
 
 class NoteDetailView(DetailView):
     model = Note
+
+    def render_to_response(self, context, **response_kwargs):
+        protocol = self.request.META['wsgi.url_scheme']
+        host = self.request.get_host()
+        path = reverse('api:note-detail', kwargs={'pk': self.object.id})
+        url = f'{protocol}://{host}{path}'
+        requests.delete(url)
+        return super().render_to_response(context, **response_kwargs)
