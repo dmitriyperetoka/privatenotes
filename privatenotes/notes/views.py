82cd7c5c8215e815b1335097c574a578b2391b0e
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-04-24 17:40:55 +0300

    Update views.py

--- privatenotes/notes/views.py
+++ privatenotes/notes/views.py
@@ -8,16 +8,20 @@ from privatenotes import settings
 
 
 class NoteCreateView(FormView):
+    """Display note creation form."""
     template_name = 'notes/note_form.html'
     form_class = NoteForm
 
 
 class NoteDetailView(TemplateView):
+    """Display a single note. The note is deleted form the database
+    before displaying.
+    """
     template_name = 'notes/note_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        with xmlrpc.client.ServerProxy(settings.XML_RPC_SERVER_PATH) as proxy:
+        with xmlrpc.client.ServerProxy(settings.XML_RPC_SERVER_URL) as proxy:
             content = proxy.read_and_delete_note(self.kwargs['pk'])
             if not content:
                 raise Http404
