4f3ce93073c668cf6a3ed9a5068152f1857ac4e0
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-04-24 21:07:08 +0300

    Update settings.py

--- privatenotes/privatenotes/settings.py
+++ privatenotes/privatenotes/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-2lga(67cz^wauvg3q&g6hrr9e1bmvq57+*!gf#yrs@&a*0(a9*'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
